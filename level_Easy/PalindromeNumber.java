965aa4df8569ee8bb154abef9e3ed4f1b5d416e7
Prashant Singh <91481645+prashant-singh-2001@users.noreply.github.com>
2023-08-12 11:03:35 +0530

    Add files via upload

---
+++ level_Easy/PalindromeNumber.java
@@ -0,0 +1,17 @@
+package level_Easy;
+
+public class PalindromeNumber {
+	public boolean isPalindrome(int x) {
+		if (x < 0)
+			return false;
+		int y = 0;
+		int z = x;
+		while (z != 0) {
+			y = y * 10 + z % 10;
+			z = z / 10;
+		}
+		if (y == x)
+			return true;
+		return false;
+	}
+}
