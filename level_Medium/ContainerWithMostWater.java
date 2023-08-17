0090db841b7b0b3c888c180f9edbbb28a9980289
Prashant Singh <91481645+prashant-singh-2001@users.noreply.github.com>
2023-08-17 12:38:39 +0530

    Add files via upload

---
+++ level_Medium/ContainerWithMostWater.java
@@ -0,0 +1,20 @@
+package level_Medium;
+
+public class ContainerWithMostWater {
+	public int maxArea(int[] height) {
+		int left = 0;
+		int right = height.length - 1;
+		int maxArea = 0;
+
+		while (left < right) {
+			int area = Math.min(height[left], height[right]) * (right - left);
+			maxArea = Math.max(maxArea, area);
+
+			if (height[left] < height[right])
+				left++;
+			else
+				right--;
+		}
+		return maxArea;
+	}
+}
